2500d35693f8320ef3e9ddb67658c1d86c7675d7
joseph19993d <rodelosuaresj@gmail.com>
2022-03-17 00:58:30 -0500

    Practicando con las rutas

--- p-w-pdp/routes/web.php
+++ p-w-pdp/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Routing\Route as RoutingRoute;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -11,8 +12,23 @@ use Illuminate\Support\Facades\Route;
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
-*/
+
 
 Route::get('/', function () {
     return view('welcome');
+
 });
+*/
+
+
+Route::get('/{pagina}',function($pagina){
+    return "Vista vacia: ".$pagina;
+});
+Route::get('/',function(){
+    return "Vista vacia";
+});
+Route::get('cursos/create',function(){ return "en esta pagina se podra crear un curso";});
+Route::get('cursos/{variable}',function($variable){ return "en esta pagina se podra crear un curso";});
+Route::get('cursos/{v1}/{v2}', function($v1,$v2){return "Las varible son[".$v1."]"."[".$v2."]";});
+
+
